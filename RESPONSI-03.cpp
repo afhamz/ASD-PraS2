da304c5c5e176f3d861b5e8c1ee9eff38ba9bdb9
Agung Pambudi <afhamz@users.noreply.github.com>
2017-05-29 14:01:20 +0700

    Update RESPONSI-03.cpp

--- RESPONSI-03.cpp
+++ RESPONSI-03.cpp
@@ -6,7 +6,7 @@ int main(){
 	cout<<"MASUKAN JUMLAH.PESERTA = ";cin>>N;
 	
 	for (i=1;i<=N;i++){
-		cout<<"MASUKAN TINGGI BADAN PESERTA = ";cin>>a[i];
+		cout<<"MASUKAN TINGGI BADAN PESERTA KE-"<<i<<" = ";cin>>a[i];
 	}
 	for (i=1;i<=N-1;i++)
   		for (j=1;j<=N-i;j++) 
